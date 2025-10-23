38cdb432d25c8859b3225cad206e4e659c5d95b2
rusok <rusok.software@gmail.com>
2025-10-23 19:54:01 -0400

    Excepciones

--- MiAppGrafica/Form1.Designer.cs
+++ MiAppGrafica/Form1.Designer.cs
@@ -34,6 +34,13 @@
             txtNumero2 = new TextBox();
             button1 = new Button();
             lblResultado = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -71,7 +78,7 @@
             // button1
             // 
             button1.ForeColor = SystemColors.ActiveCaption;
-            button1.Location = new Point(148, 102);
+            button1.Location = new Point(45, 101);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 2;
@@ -82,17 +89,81 @@
             // lblResultado
             // 
             lblResultado.AutoSize = true;
-            lblResultado.Location = new Point(45, 143);
+            lblResultado.Location = new Point(45, 160);
             lblResultado.Name = "lblResultado";
             lblResultado.Size = new Size(62, 15);
             lblResultado.TabIndex = 3;
             lblResultado.Text = "Resultado:";
             // 
+            // button2
+            // 
+            button2.Location = new Point(148, 101);
+            button2.Name = "button2";
+            button2.Size = new Size(93, 23);
+            button2.TabIndex = 4;
+            button2.Text = "RESTAR";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += restar;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(45, 130);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 5;
+            button3.Text = "DIVIDIR";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += unMetodo;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(148, 130);
+            button4.Name = "button4";
+            button4.Size = new Size(93, 23);
+            button4.TabIndex = 6;
+            button4.Text = "MULTIPLICAR los numeros";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(247, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(537, 532);
+            tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(529, 504);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(192, 72);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(265, 556);
+            ClientSize = new Size(796, 556);
+            Controls.Add(tabControl1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(lblResultado);
             Controls.Add(button1);
             Controls.Add(txtNumero2);
@@ -101,6 +172,7 @@
             Controls.Add(label1);
             Name = "Form1";
             Text = "Ejemplos actuales";
+            tabControl1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -113,5 +185,11 @@
         private TextBox txtNumero2;
         private Button button1;
         private Label lblResultado;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
--- MiAppGrafica/Form1.cs
+++ MiAppGrafica/Form1.cs
@@ -20,7 +20,74 @@ namespace MiAppGrafica
             else
                 lblResultado.Text = "POR FAVOR INGRESE SOLO NUMEROS";
         }
-        public void un«Metodo() { 
+        public void unMetodo(object sender, EventArgs e)
+        {
+            int num1 = 0, num2 = 0;
+            try
+            {
+                num1 = int.Parse(txtNumero1.Text);
+                num2 = int.Parse(txtNumero2.Text);
+                txtNumero1.Text = "";
+                txtNumero2.Text = "";
+                lblResultado.Text = "Resultado: " + (num1 / num2);
+            }
+            catch (FormatException e1)
+            {
+                lblResultado.Text = "POR FAVOR INGRESE SOLO NUMEROS";
+                return;
+            }
+            catch (DivideByZeroException e2)
+            {
+                lblResultado.Text = "NO SE PUEDE DIVIDIR POR CERO";
+                return;
+            }
+            catch (Exception e3)
+            {
+                lblResultado.Text = "OCURRIO UN ERROR INESPERADO: " + e3.Message;
+                return;
+            }
+
+        }
+
+        private void restar(object sender, EventArgs e)
+        {
+            int num1 = 0, num2 = 0;
+            try
+            {
+                num1 = int.Parse(txtNumero1.Text);
+                num2 = int.Parse(txtNumero2.Text);
+            }
+            catch (FormatException e1)
+            {
+                lblResultado.Text = "POR FAVOR INGRESE SOLO NUMEROS";
+                return;
+            }
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            lblResultado.Text = "Resultado: " + (num1 - num2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num1 = 0, num2 = 0;
+            try
+            {
+                num1 = int.Parse(txtNumero1.Text);
+                num2 = int.Parse(txtNumero2.Text);
+            }
+            catch (FormatException e1)
+            {
+                lblResultado.Text = "POR FAVOR INGRESE SOLO NUMEROS";
+                return;
+            }
+            catch (Exception e3)
+            {
+                lblResultado.Text = "OCURRIO UN ERROR INESPERADO: " + e3.Message;
+                return;
+            }
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            lblResultado.Text = "Resultado: " + (num1 * num2);
         }
     }
 }
